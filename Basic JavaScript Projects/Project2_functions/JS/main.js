efe3c7a77e58a41793daa356ddf7e7900f6709e1
JBowersock <jbowersock22@hotmail.com>
2022-03-09 21:03:34 -0500

    Step 66 and 68 Issues

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,18 @@
+//Below is Step 66 - I got confused by #2 & 8 on your site. This is what i have.
+function Two_Variables() {
+    var A="Apples"; //Assigned first variable.
+    var B="Oranges"; // Assigned second variable.
+    document.getElementById("Two_Variables").innerHTML=
+}
+
+function My_First_Function() { //Defined a function.
+    var str="This is the button text!"; //Defined variable, gave it a string value.
+    document.getElementById("Button_Text").innerHTM=str; // Put value of the variable into the HTML element
+}
+
+//Below is Step 68
+function myFunction() { //Defined a function.
+    var sentence="I am learning";
+    sentence+=" a lot from this book!";
+    document.getElementById("Concatenate").innerHTML=sentence; //Put value of variable into HTML element
+}
