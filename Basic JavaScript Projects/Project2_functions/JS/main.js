1a195016b90e5a9a697ea695dac2adb3a8bafe38
JBowersock <jbowersock22@hotmail.com>
2022-03-10 15:32:51 -0500

    Step 66 & 68 Correction

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,13 +1,13 @@
-//Below is Step 66 - I got confused by #2 & 8 on your site. This is what i have.
+//Below is Step 66
 function Two_Variables() {
     var A="Apples"; //Assigned first variable.
     var B="Oranges"; // Assigned second variable.
-    document.getElementById("Two_Variables").innerHTML=
+    document.getElementById("Two_Variables").innerHTML=A;
 }
 
 function My_First_Function() { //Defined a function.
     var str="This is the button text!"; //Defined variable, gave it a string value.
-    document.getElementById("Button_Text").innerHTM=str; // Put value of the variable into the HTML element
+    document.getElementById("Button_Text").innerHTML=str; // Put value of the variable into the HTML element
 }
 
 //Below is Step 68
